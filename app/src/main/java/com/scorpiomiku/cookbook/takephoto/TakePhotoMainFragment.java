7a589ed9b1e968a91e954d446203c520c17c0426
wang <1056992492@qq.com>
2017-06-06 17:37:36 +0800

    takephoto

--- app/src/main/java/com/scorpiomiku/cookbook/takephoto/TakePhotoMainFragment.java
+++ app/src/main/java/com/scorpiomiku/cookbook/takephoto/TakePhotoMainFragment.java
@@ -1,10 +1,14 @@
 package com.scorpiomiku.cookbook.takephoto;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.scorpiomiku.cookbook.R;
 import com.scorpiomiku.cookbook.module.FragmentModule;
@@ -14,6 +18,11 @@ import com.scorpiomiku.cookbook.module.FragmentModule;
  */
 
 public class TakePhotoMainFragment extends FragmentModule {
+
+    private Button mTakePhotoButton;
+    private boolean canTakePhoto;
+    private Intent mTakePhotoIntent;
+
     public static TakePhotoMainFragment newInstance() {
         return new TakePhotoMainFragment();
     }
@@ -21,12 +30,28 @@ public class TakePhotoMainFragment extends FragmentModule {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mTakePhotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        canTakePhoto = mTakePhotoIntent.resolveActivity(getActivity().getPackageManager()) != null;
+
     }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.takephoto_fragment_layout, container, false);
+        mTakePhotoButton = (Button) v.findViewById(R.id.takephoto_take_photo_button);
+        mTakePhotoButton.setEnabled(canTakePhoto);
+        setListener();
         return v;
+    }
+
+    /*---------------------------setListener-----------------------*/
+    private void setListener() {
+        mTakePhotoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               startActivity(mTakePhotoIntent);
+            }
+        });
     }
 }

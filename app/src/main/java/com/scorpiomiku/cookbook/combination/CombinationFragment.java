720a2148c48755561c21e7097f2b7b280a6e129f
wang <1056992492@qq.com>
2017-07-04 18:38:47 +0800

    1

--- app/src/main/java/com/scorpiomiku/cookbook/combination/CombinationFragment.java
+++ app/src/main/java/com/scorpiomiku/cookbook/combination/CombinationFragment.java
@@ -5,6 +5,7 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 
 import com.scorpiomiku.cookbook.R;
 import com.scorpiomiku.cookbook.module.FragmentModule;

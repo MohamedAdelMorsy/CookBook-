e7be46a461ab09cf5433ec10e114d5b379c175ae
wang <1056992492@qq.com>
2017-07-14 17:08:47 +0800

    1

--- app/src/main/java/com/scorpiomiku/cookbook/recommendmenufragment/MenuAdapter.java
+++ app/src/main/java/com/scorpiomiku/cookbook/recommendmenufragment/MenuAdapter.java
@@ -1,6 +1,9 @@
 package com.scorpiomiku.cookbook.recommendmenufragment;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,18 +20,23 @@ import java.util.List;
 public class MenuAdapter extends RecyclerView.Adapter<MenuHolder> {
     private List<String> mList;
     private Context mContext;
+    private Drawable mClickedDrawable;
+    private Drawable mUnClickedDrawable;
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     public MenuAdapter(List<String> list, Context context) {
         super();
         mList = list;
         mContext = context;
+        mClickedDrawable = mContext.getDrawable(R.drawable.recommend_menu_item_circle_clicked);
+        mUnClickedDrawable = mContext.getDrawable(R.drawable.recommend_menu_item_circle_unclicked);
     }
 
     @Override
     public MenuHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         LayoutInflater layoutInflater = LayoutInflater.from(mContext);
         View v = layoutInflater.inflate(R.layout.recommend_menu_recycler_view_item, parent, false);
-        return new MenuHolder(v);
+        return new MenuHolder(v, mUnClickedDrawable, mClickedDrawable);
     }
 
     @Override
--- app/src/main/java/com/scorpiomiku/cookbook/recommendmenufragment/MenuHolder.java
+++ app/src/main/java/com/scorpiomiku/cookbook/recommendmenufragment/MenuHolder.java
@@ -1,5 +1,7 @@
 package com.scorpiomiku.cookbook.recommendmenufragment;
 
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.TextView;
@@ -12,12 +14,30 @@ import com.scorpiomiku.cookbook.R;
 
 public class MenuHolder extends RecyclerView.ViewHolder {
     private TextView mTextView;
+    private boolean isClicked = false;
+    private Drawable mClickedDrawable;
+    private Drawable mUnClickedDrawable;
 
-    public MenuHolder(View itemView) {
+    public MenuHolder(View itemView, final Drawable unClicked, final Drawable clicked) {
         super(itemView);
+        mClickedDrawable = clicked;
+        mUnClickedDrawable = unClicked;
         mTextView = (TextView) itemView.findViewById(R.id.recommend_menu_text_view);
+        mTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!isClicked) {
+                    mTextView.setBackground(clicked);
+                    isClicked = true ;
+                }else{
+                    mTextView.setBackground(unClicked);
+                    isClicked = false ;
+                }
+            }
+        });
     }
-    public void bindView(String s){
+
+    public void bindView(String s) {
         mTextView.setText(s);
     }
 }

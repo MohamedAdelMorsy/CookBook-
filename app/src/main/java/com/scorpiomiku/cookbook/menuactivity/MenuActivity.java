3fb24e7f70c0e31cc8bb7c68f6ff4af16e56c2bf
wang <1056992492@qq.com>
2017-07-22 17:35:03 +0800

    clock

--- app/src/main/java/com/scorpiomiku/cookbook/menuactivity/MenuActivity.java
+++ app/src/main/java/com/scorpiomiku/cookbook/menuactivity/MenuActivity.java
@@ -1,5 +1,8 @@
 package com.scorpiomiku.cookbook.menuactivity;
 
+import android.app.AlertDialog;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
@@ -8,12 +11,16 @@ import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.scorpiomiku.cookbook.R;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MenuActivity extends AppCompatActivity {
-    
-    private String TAG = "MenuActivity" ;
+
+    private String TAG = "MenuActivity";
 
     private ImageView mDishImageView;
     private TextView mDishNameTextView;
@@ -22,6 +29,13 @@ public class MenuActivity extends AppCompatActivity {
     private TextView mNutritionTextView;
     private ImageView mClockImageView;
     private TextView mLookAllTextView;
+
+    private int mTimeCount = 0;
+    private Timer timer = new Timer();
+    private TimerTask mTimerTask = null;
+
+    private static final int MSG_WHAT_TIME_IS_UP = 1;//时间到了
+    private static final int MSG_WHAT_TIME_IS_TICK = 2;//时间减少中
 
     private FragmentManager fm;
 
@@ -59,10 +73,54 @@ public class MenuActivity extends AppCompatActivity {
         mDishNameTextView = (TextView) findViewById(R.id.menu_fragment_text_view_dish_name);
         mDishMaterialsTextView = (TextView) findViewById(R.id.menu_fragment_text_view_dish_materials);
         mLookAllTextView = (TextView) findViewById(R.id.menu_activity_lookall_text_view);
+        mClockImageView = (ImageView) findViewById(R.id.menu_alarm_clock_image_view);
         fm = getSupportFragmentManager();
         initView();
         setListener();
     }
+    /*----------------------------------Clorck----------------------------------*/
+
+
+    private void startTimer() {
+        if (mTimerTask == null) {
+            mTimeCount = 5;
+            Toast.makeText(MenuActivity.this, "测试时间为5秒", Toast.LENGTH_SHORT).show();
+            mTimerTask= new TimerTask() {
+                @Override
+                public void run() {
+                    mTimeCount--;
+                    handler.sendEmptyMessage(MSG_WHAT_TIME_IS_TICK);
+                    if (mTimeCount <= 0) {   //时间到了就弹出对话框
+                        handler.sendEmptyMessage(MSG_WHAT_TIME_IS_UP);
+                        stopTimer();
+                    }
+                }
+            };
+            timer.schedule(mTimerTask, 1000, 1000);
+        }
+    }
+
+    private void stopTimer() {
+        if (mTimerTask != null) {
+            mTimerTask.cancel();
+            mTimerTask = null;
+        }
+    }
+
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            switch (msg.what) {
+                case MSG_WHAT_TIME_IS_UP:
+
+                    break;
+                case MSG_WHAT_TIME_IS_TICK://显示动态时间
+
+                    break;
+            }
+        }
+    };
 
     /*---------------------------------initView-------------------------------*/
     private void initView() {
@@ -81,9 +139,9 @@ public class MenuActivity extends AppCompatActivity {
         mLookAllTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(isFragmentFooter()){
+                if (isFragmentFooter()) {
                     changeFragment(DefaultFragment.newInstance());
-                }else{
+                } else {
                     changeFragment(MenuRecyclerStepsFragment.newInstance());
                 }
             }
@@ -94,9 +152,9 @@ public class MenuActivity extends AppCompatActivity {
     /*--------------------------------------Back----------------------*/
     @Override
     public void onBackPressed() {
-        if(isFragmentFooter()){
+        if (isFragmentFooter()) {
             changeFragment(DefaultFragment.newInstance());
-        }else {
+        } else {
             super.onBackPressed();
         }
     }
